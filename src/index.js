947638fed2ea1a3558c9cb6d03909d1317faa63d
root <root@DESKTOP-GSIMMEL.localdomain>
2020-08-10 12:37:14 +0530

    adding files

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import MyResume from './assets/Arjun_Resume.pdf';
+import initTilt from './js/tilt';
+import initSr from './js/sr';
+
+import './style/main.scss';
+
+$('a[href^="#"]').on('click', function(event) {
+  var target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+  }
+});
+
+
+function addResume(MyResume){
+  const elements = document.getElementsByClassName('.cta-btn--resume');
+  for(let el of elements){
+    el.href = url(MyResume);
+  }
+}initSr();
+initTilt();
+addResume();
